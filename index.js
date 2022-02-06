f06ac4adb9624d63fd7d5a1b9e5729d93ef68fc6
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-06 17:26:22 -0600

    commit tree 77fe80b4ef34864b28698babf15548431176c386

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("The DOM has Loaded");
+    function changingText(newText) {
+        let text = document.getElementById("text");
+        text.textContent = newText;
+    }
+    console.log(changingText("This is really cool!"))
+});
+
+console.log("Text before DOM has loaded")
